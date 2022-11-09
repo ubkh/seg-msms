bd364d6dad9561f7840b8da2dbe2653d90fb29c0
Matushan Yogaraj <dev@matushan.com>
2022-11-09 00:07:13 +0000

    Adapt admin interface to display list of users

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,13 @@
+"""Configuration of the admin interface for the music school management system."""
+
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface to display users."""
+    list_display = [
+        'id', 'email', 'name', 'last_login', 'is_staff', 'is_superuser'
+    ]
