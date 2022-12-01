0c109a69fa592009f311cbb8c88e7b81e1b0c46d
Matushan Yogaraj <dev@matushan.com>
2022-12-01 08:17:03 +0000

    Create Mixin to restrict views to a specified group

--- lessons/views/transfer_view.py
+++ lessons/views/transfer_view.py
@@ -1,33 +1,32 @@
 """
 Views that will be used in the music school management system.
 """
-
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import ListView, CreateView
 
 from lessons.forms import TransferForm
+from lessons.mixins import GroupRestrictedMixin
 from lessons.models import Transfer
-from lessons.helpers import administrator_restricted
 
 
-# @administrator_restricted
-class TransferListView(LoginRequiredMixin, ListView):
-
+class TransferListView(LoginRequiredMixin, GroupRestrictedMixin, ListView):
     model = Transfer
     template_name = "transfer/transfers.html"
     context_object_name = "transfers"
+    allowed_group = "Administrator"
+
+    def handle_no_permission(self):
+        return redirect('home')
 
 
-# @administrator_restricted
-class TransferCreateView(LoginRequiredMixin, CreateView):
-
+class TransferCreateView(LoginRequiredMixin, GroupRestrictedMixin, CreateView):
     model = Transfer
     template_name = "transfer/record_transfer.html"
     form_class = TransferForm
     http_method_names = ['get', 'post']
+    allowed_group = "Administrator"
 
     def get_success_url(self):
         return reverse('transfers')
